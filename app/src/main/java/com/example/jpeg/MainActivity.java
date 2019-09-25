56c5099fad26e95cfb3c39846f1d860b3bfeaac9
kin16 <nikbatishov114@gmail.com>
2019-09-25 16:50:35 +0500

    Update backend

--- app/src/main/java/com/example/jpeg/MainActivity.java
+++ app/src/main/java/com/example/jpeg/MainActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.Environment;
 import android.os.Handler;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -16,19 +17,21 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     private final int PERMISSIONS_RECORD_AUDIO = 1;
     private final int PERMISSIONS_WRITE_STORAGE = 2;
+    private final int PERMISSIONS_READ_STORAGE = 3;
     private Sound sound = new Sound();
     private EditText mEditText;
     private Handler handler = new Handler();
-    private MainActivity mainActivity = this;
+    private MainActivity activity = this;
+    private double lastAmp = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         mEditText = findViewById(R.id.input);
-        mEditText.setText(Environment.getExternalStorageDirectory() + "/records/record.3gpp");
+        mEditText.setText(Environment.getExternalStorageDirectory() + "/records/record.aac");
         sound.mkdir();
     }
 
@@ -36,15 +39,18 @@ public class MainActivity extends AppCompatActivity {
     public void onClick(View v){
         switch (v.getId()){
             case R.id.write:
-                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_WRITE_STORAGE);
+                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSIONS_READ_STORAGE);
                 }
-                sound.write(mEditText.getText().toString(), this);
+                sound.preWrite(mEditText.getText().toString(), this);
+                break;
             case R.id.start_record:
                 if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
                     ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO}, PERMISSIONS_RECORD_AUDIO);
                 }
-
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSIONS_WRITE_STORAGE);
+                }
                 sound.recordStart();
                 handler.post(mPollTask);
                 break;
@@ -52,7 +58,7 @@ public class MainActivity extends AppCompatActivity {
                 sound.recordStop();
                 break;
             case R.id.start_play:
-                sound.playStart();
+                sound.playStart(this);
                 break;
             case R.id.stop_play:
                 sound.playStop();
@@ -63,14 +69,14 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             try {
                 double amp = sound.getAmplitude();
-
-                if ((amp > 0)) {
+                if (amp > 0 && (amp - lastAmp) > 2) {
                     synchronized (this) {
                         wait(300);
                     }
                     sound.recordStop();
-                    Toast.makeText(mainActivity, "Остановлено", Toast.LENGTH_LONG).show();
+                    Toast.makeText(activity, "Остановлено", Toast.LENGTH_LONG).show();
                 } else {
+                    lastAmp = amp;
                     handler.postDelayed(mPollTask, 20);
                 }
             }catch (InterruptedException e){
--- app/src/main/java/com/example/jpeg/Sound.java
+++ app/src/main/java/com/example/jpeg/Sound.java
@@ -3,19 +3,27 @@ package com.example.jpeg;
 import android.content.Context;
 import android.media.MediaPlayer;
 import android.media.MediaRecorder;
+import android.os.AsyncTask;
 import android.os.Environment;
+import android.util.Log;
 import android.widget.Toast;
+
+import com.github.hiteshsondhi88.libffmpeg.FFmpeg;
+import com.github.hiteshsondhi88.libffmpeg.FFmpegExecuteResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.FFmpegLoadBinaryResponseHandler;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegCommandAlreadyRunningException;
+import com.github.hiteshsondhi88.libffmpeg.exceptions.FFmpegNotSupportedException;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+
 
 
 public class Sound {
     private MediaRecorder mediaRecorder;
     private MediaPlayer mediaPlayer;
-    private String fileName = Environment.getExternalStorageDirectory() + "/records/record_pattern.3gpp";
+    private String fileName = Environment.getExternalStorageDirectory() + "/records/record_pattern.aac";
     private File outFile;
+    private File file;
+    private static String TAG = "Sound";
 
     public void recordStart() {
         try {
@@ -28,8 +36,8 @@ public class Sound {
 
             mediaRecorder = new MediaRecorder();
             mediaRecorder.setAudioSource(MediaRecorder.AudioSource.MIC);
-            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.THREE_GPP);
-            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AMR_NB);
+            mediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+            mediaRecorder.setAudioEncoder(MediaRecorder.AudioEncoder.AAC);
             mediaRecorder.setOutputFile(fileName);
             mediaRecorder.prepare();
             mediaRecorder.start();
@@ -45,11 +53,19 @@ public class Sound {
         }
     }
 
-    public void playStart() {
+    public void playStart(Context context) {
         try {
             releasePlayer();
             mediaPlayer = new MediaPlayer();
-            mediaPlayer.setDataSource(fileName);
+            if(file == null){
+                Toast.makeText(context, "Запишите аудио", Toast.LENGTH_LONG).show();
+            }
+            mediaPlayer.setDataSource(file.getAbsolutePath());
+            if (mediaPlayer.isPlaying()) {
+                mediaPlayer.stop();
+                mediaPlayer.reset();
+                mediaPlayer.release();
+            }
             mediaPlayer.prepare();
             mediaPlayer.start();
         } catch (Exception e) {
@@ -77,35 +93,100 @@ public class Sound {
         }
     }
 
-    public void write(String path, Context context){
+    public void preWrite(String path, Context context){
         mkdir();
-        if(path.equals(null) || path.equals("")){
+        if (path == null || path.equals("")) {
             Toast.makeText(context,"Введить путь для файла", Toast.LENGTH_LONG).show();
-        }else if(new File(path).exists()) {
+        } else if (new File(path).exists()) {
             Toast.makeText(context,"Такой файл уже существует", Toast.LENGTH_LONG).show();
-        }else if(outFile == null){
+        } else if (outFile == null) {
             Toast.makeText(context,"Запишите аудио", Toast.LENGTH_LONG).show();
-        }else {
-            File file = new File(path);
-            try(FileInputStream fin=new FileInputStream(outFile);
-                FileOutputStream fos=new FileOutputStream(file))
-            {
-                /*byte[] buffer = new byte[fin.available()];
-                // считываем буфер
-                fin.read(buffer, 0, buffer.length);
-                // записываем из буфера в файл
-                fos.write(buffer, 0, buffer.length);
-                 */
-                int min = getDuration() - 350;
-                String[] ffmpeg = new String[] {"ffmpeg", "-i", outFile.getPath(),"-ss", "0."+min, "-t", "0."+getDuration(),"-async","1", "-c", "copy", file.getPath()};
-                Process p = Runtime.getRuntime().exec(ffmpeg);
-                Toast.makeText(context,"Сохранено", Toast.LENGTH_SHORT).show();
-            }
-            catch(IOException ex){
-                System.out.println(ex.getMessage());
-            }
+        } else {
+            file = new File(path);
+            new MyTask(context).execute();
+            Log.d(TAG, "Start write");
+            Toast.makeText(context,"Сохранено", Toast.LENGTH_LONG).show();
         }
     }
+
+    private void write(Context context) {
+            FFmpeg fFmpeg = FFmpeg.getInstance(context);
+            try {
+                fFmpeg.loadBinary(new FFmpegLoadBinaryResponseHandler() {
+
+                    @Override
+                    public void onFailure() {
+                        Log.i("Load", "on failure");
+                    }
+
+                    @Override
+                    public void onSuccess() {
+                        Log.i("Load", "success");
+                    }
+
+                    @Override
+                    public void onStart() {
+                        Log.i("Load", "on start");
+                    }
+                    @Override
+                    public void onFinish() {
+                        Log.i("Load", "on finish");
+                    }
+                });
+                int duration = getDuration();
+                int mduration = duration - 350;
+                int min = mduration / 1000;
+                int max = duration / 1000;
+                duration = duration - (max * 1000);
+                mduration = mduration - (min * 1000);
+                int mx = 0;
+                if(max > 10){
+                    mx = max / 10;
+                    max = max - (mx * 10);
+                }
+                int mn = 0;
+                if(min > 10){
+                    mn = min / 10;
+                    min = min - (mn * 10);
+                }
+                fFmpeg.execute(new String[]{"-y", "-i", outFile.getAbsolutePath(),
+                        "-ss", "00:00:"+mn+""+min+"."+mduration,
+                        "-t", "00:00:"+mx+""+max+"."+duration,
+                        "-async","1", "-c", "copy", file.getAbsolutePath()},
+                        new FFmpegExecuteResponseHandler() {
+                    @Override
+                    public void onSuccess(String message) {
+                        Log.i("FFmpeg", message);
+                    }
+                    @Override
+                    public void onProgress(String message) {
+                        String[] strings = message.split("\n");
+                        for (String string : strings) {
+                            Log.i("FFmpeg", string);
+                        }
+                    }
+                    @Override
+                    public void onFailure(String message) {
+                        String[] strings = message.split("\n");
+                        for (String string : strings) {
+                            Log.e("FFmpeg", string);
+                        }
+                    }
+                    @Override
+                    public void onStart() {
+                        Log.i("FFmpeg", "on start");
+                    }
+                    @Override
+                    public void onFinish() {
+                        Log.i("FFmpeg", "on finish");
+                    }
+                });
+            } catch (FFmpegNotSupportedException e) {
+                e.printStackTrace();
+            } catch (FFmpegCommandAlreadyRunningException e) {
+                e.printStackTrace();
+            }
+        }
 
     private int getDuration() {
         //узнаем продолжительность записи
@@ -115,18 +196,18 @@ public class Sound {
             mplayer.setDataSource(fileName);
             mplayer.prepare();
             duration = mplayer.getDuration();
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
         return duration;
     }
 
-    public void mkdir(){
+    public void mkdir() {
         File theDir = new File(Environment.getExternalStorageDirectory() + "/records");
         //создание папки, если отсутсвует
         if (!theDir.exists()) {
-            System.out.println("creating directory: " + theDir.getName());
+            Log.d(TAG, "creating directory: " + theDir.getName());
             theDir.mkdir();
         }
     }
@@ -134,8 +215,21 @@ public class Sound {
     public double getAmplitude() {
         if (mediaRecorder != null)
             //вычисление амплитуды
-            return   20 * Math.log10(mediaRecorder.getMaxAmplitude() / 2700.0);
+            return 20 * Math.log10(mediaRecorder.getMaxAmplitude() / 2700.0);
         else
             return 0;
+    }
+
+    private class MyTask extends AsyncTask<Void, Void, Void> {
+        private Context c;
+        public MyTask(Context context){
+            c = context;
+        }
+
+        @Override
+        protected Void doInBackground(Void... voids) {
+            write(c);
+            return null;
+        }
     }
 }
